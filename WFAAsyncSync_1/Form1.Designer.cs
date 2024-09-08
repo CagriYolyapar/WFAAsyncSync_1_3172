2fd4f84e4ae67284fe31a8b0c58de4bf3803a134
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-09-08 14:30:17 +0300

    Add project files.

---
+++ WFAAsyncSync_1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WFAAsyncSync_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnCalistir = new Button();
+            BtnSayi = new Button();
+            SuspendLayout();
+            // 
+            // BtnCalistir
+            // 
+            BtnCalistir.Location = new Point(83, 59);
+            BtnCalistir.Name = "BtnCalistir";
+            BtnCalistir.Size = new Size(94, 29);
+            BtnCalistir.TabIndex = 0;
+            BtnCalistir.Text = "Calıstır";
+            BtnCalistir.UseVisualStyleBackColor = true;
+            BtnCalistir.Click += BtnCalistir_Click;
+            // 
+            // BtnSayi
+            // 
+            BtnSayi.Location = new Point(455, 74);
+            BtnSayi.Name = "BtnSayi";
+            BtnSayi.Size = new Size(94, 29);
+            BtnSayi.TabIndex = 1;
+            BtnSayi.Text = "Sayi";
+            BtnSayi.UseVisualStyleBackColor = true;
+            BtnSayi.Click += BtnSayi_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(BtnSayi);
+            Controls.Add(BtnCalistir);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button BtnCalistir;
+        private Button BtnSayi;
+    }
+}
---
+++ WFAAsyncSync_1/Form1.cs
@@ -0,0 +1,76 @@
+﻿namespace WFAAsyncSync_1
+{
+    public partial class Form1 : Form
+    {
+
+        //Bir metodun asenkron calısabilmesini istiyorsanız o metot geriye deger döndürse de döndürmese de Task tipi ile yaratılmalıdır... Task tipi yaratıldıktan sonra geriye deger döndürmeyecekse Task'e baska bir şey yazılmaz....Eger geriye deger döndürecekse o deger tipi Task'e generic olarak verilir...
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+
+        Task<int> SelamlaAsync()
+        {
+            
+            return Task.Run(
+                //Selamla => metodumuzu bu sekilde parantezsiz de verebiliriz
+
+                //Veya anonim bir metot verebiliriz
+                () =>
+                {
+                    Thread.Sleep(15000);
+                    //MessageBox.Show("Async olarak cagrıldıgım icin kimseyi engellemedim");
+                    return 100;
+                });
+        }
+
+
+        public void Selamla()
+        {
+            Thread.Sleep(5000);
+            MessageBox.Show("Hello World");
+        }
+
+
+        public int KareAl(int sayi)
+        {
+            Thread.Sleep(10000);
+            return sayi * sayi;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void BtnCalistir_Click(object sender, EventArgs e)
+        {
+           Task<int> a =   SelamlaAsync(); //Eger Asenkron bir metot await ile cagrılmazsa ve tek basına cagrılırsa program o metodu beklemeden ilerlemeye devam edecektir...Bu da metodun ilgili görevinin yapılmasını bypass ederek baska kodları calıstırabilir ve o kodlar da eger önceki metodun görevine baglıysa problem yasanır...Dolayısıyla asenkron metotların await keyword'u ile cagrılıp görevlerinin tamamlanması beklenmelidir...await keyword'unun cagrılması icin ise icerisinde kod yazdıgınız metodun async keyword'u ile işaretlenmesi gerekir...
+            int b = a.Result;
+            MessageBox.Show(b.ToString());
+
+
+
+
+
+
+        }
+
+        private void BtnSayi_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(2.ToString());
+        }
+
+
+
+
+
+
+    }
+}
